76d31ae67266ee9194086ecd6227317be225c6cd
Mike Lipatov <misha.lipatov@gmail.com>
2019-10-10 15:07:42 -0700

    different spectra now go to separate files

--- pa/calc_spectra.py
+++ pa/calc_spectra.py
@@ -4,24 +4,26 @@ from pa.lib import fit as ft
 from pa.lib import star
 from pa.lib import util as ut
 import numpy as np
+from numpy.core import defchararray as ch
 import sys
 import time
 import argparse
 import pickle
+import os
 
 def run():
 	parser = argparse.ArgumentParser(description="Example: \n" +\
-		"calc_spectra \'data/vega.pkl\' \'data/vega.txt\' -i 0 1.5707963267948966 0.01; " +\
-		"calc_spectra \'data/vega.pkl\' \'data/vega.txt\' -i 0.08683")
+		"calc_spectra \'data/vega.pkl\' \'data/vega/\' -i 0 1.5707963267948966 0.01; " +\
+		"calc_spectra \'data/vega.pkl\' \'data/vega/\' -i 0.08683")
 	parser.add_argument("pkl_sfile", help="the pickled star file")
-	parser.add_argument("output", help="an output spectrum text file to create")
+	parser.add_argument("output", help="an output directory")
 	parser.add_argument('-i', type=float, nargs='+', help='either a single inclination in radians ' +
 		'or a range specified by minimum, maximum and step', required=True)
 	args = parser.parse_args()
 
 	## inputs
-	txt_sfile = args.output # spectrum text file
-	pkl_sfile = args.pkl_sfile # pickled star
+	pkl_sfile = args.pkl_sfile # pickled star file
+	output = args.output # output location
 	i = args.i # inclinations
 	li = len(i)
 	if li not in [1, 3]:
@@ -31,44 +33,51 @@ def run():
 		inclinations = i
 	elif li == 3:
 		inclinations = np.arange(*i)
+		# decimal precision of inclination
+		prec = np.int( np.ceil( -np.log10( i[2] ) ) )
+	leni = len(inclinations)
 	# unpickle the star
 	with open(pkl_sfile, 'rb') as f:
 		st = pickle.load(f)
 	# get the wavelengths at which we see light from this star
 	wl = st.wavelengths
 
-	## write the spectrum of the star in text format
-	# create this file if it doesn't exist, open it for writing
-	f = open(txt_sfile,'w+') 
-	# write the header
-	f.write('# omega: ' + str(st.surface.omega) + '\n')
-	f.write('# luminosity: ' + str(st.luminosity) + '\n')
-	f.write('# mass: ' + str(st.mass) + '\n')
-	f.write('# Req: ' + str(st.Req) + '\n')
-	f.write('# z resolution: ' + str(st.map.dz) + '\n')
-	f.close() # close the file
+	## write the spectra of the star in text format
+	# create the directory
+	if not os.path.exists(output):
+		os.mkdir(output)
+	# filenames
+	if not output.endswith('/'):
+		output += '/'
+	filename = os.path.splitext(os.path.basename(pkl_sfile))[0]
+	ofiles = ch.add(output + filename, np.round(inclinations, decimals=prec).astype(str))
+	ofiles = ch.replace(ofiles, '.', '_')
+	ofiles = ch.add(ofiles, '.txt')
 
-	# open the file for appending
-	f = open(txt_sfile, 'a') 
-	# calculate the spectra
-	leni = len(inclinations)
-	light = np.empty( (leni, len(wl)) )
-	for i, inc in np.ndenumerate(inclinations):
-		light[i] = st.integrate(inc)
+	for i, ofile in np.ndenumerate(ofiles):
+		# message
 		if i[0] % 10 == 0:		
 			print(str(i[0]) + " out of " + str(leni) + " inclinations calculated.")        
 			sys.stdout.flush()
+		# current inclination
+		inc = inclinations[i] 
+		# calculate the spectrum
+		light = st.integrate(inc)
 
-	# write the spectra to file
-	f.write('\n')
-	f.write('# intensity(ergs/s/Hz/ster) \n')
-	f.write('# wavelength(nm)\\inclination(rad)') 
-	for i, inc in np.ndenumerate(inclinations):
-		f.write( ' \t' + str(inc) )
-	f.write('\n')
-	for j, w in np.ndenumerate(wl):
-		f.write( str(w) )
-		for i, inc in np.ndenumerate(inclinations):
-			f.write('\t %.5E' % light[i, j])
+		# create this file if it doesn't exist, open it for writing
+		f = open(ofile,'w+') 
+		# write the header
+		f.write('# omega: ' + str(st.surface.omega) + '\n')
+		f.write('# luminosity: ' + str(st.luminosity) + '\n')
+		f.write('# mass: ' + str(st.mass) + '\n')
+		f.write('# Req: ' + str(st.Req) + '\n')
+		f.write('# z resolution: ' + str(st.map.dz) + '\n')
+		f.write('# inclination(rad): ' + str(inclinations[i]) + '\n')
+		# write the spectrum to the file
 		f.write('\n')
-	f.close()
+		f.write('# wavelength(nm)\tintensity(ergs/s/Hz/ster)\n') 
+		for j, w in np.ndenumerate(wl):
+			f.write( str(w) )
+			f.write('\t %.5E' % light[j])
+			f.write('\n')
+		f.close()
