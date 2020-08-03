a19dd3a227d06adb16aee0d5024b20183daf541e
Mike Lipatov <misha.lipatov@gmail.com>
2020-08-03 10:29:43 -0700

    simplified the grid class

--- pa/opt/grid.py
+++ pa/opt/grid.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Grid:
 	""" Absolute magnitudes for a grid of stars with sun's equatorial radius"""
-	def __init__(self, tau, omega, inc, gamma, Z, av, bands):
+	def __init__(self, tau, omega, inc, gamma, Z, av, bands, Mag):
 		# stellar model parameters
 		self.tau = tau
 		self.omega = omega
@@ -14,7 +14,4 @@ class Grid:
 		self.av = av
 		self.bands = bands
 
-		# magnitudes, indexed in the above order 
-		# use a less memory-intensive data type
-		shape = tuple(( len(tau), len(omega), len(inc), len(gamma), len(Z), len(av), len(bands) ))
-		self.Mag = np.full( shape, np.nan, dtype=np.float32 )
+		self.Mag = Mag
